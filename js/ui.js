7fec33e44bec108d20191658449be03ae2e58374
abdalle_25 <abdalle.abdulkadir@chasacademy.se>
2026-06-17 15:56:56 +0200

    Implement responsive design

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,10 @@
 const data = JSON.parse(localStorage.getItem("searchData"));
 
 document.querySelector("#results").innerHTML = `
+  <div class="result-card">
+  <h2>Travel Results</h2>
     <p>Budget: ${data.budget}</p>
     <p>Days: ${data.days}</p>
     <p>Departure Date: ${data.departureDate}</p>
+  </div>
 `;
